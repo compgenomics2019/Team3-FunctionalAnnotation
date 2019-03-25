0201133bb90854f46e21437ab4d53a116bf34181
Sharma, Siddhartha <ssharma435@gatech.edu>
2019-03-25 18:54:28 -0400

    Update pipeline.bash

--- pipeline.bash
+++ pipeline.bash
@@ -124,6 +124,8 @@ ab_initio() {
 	
 	# SignalP
 	# What is the output?
+	# The prefix argument takes in a user input for name of the output file and appends a .gff3 at the end of the specified name.
+	# Here if the prefix is signalpOut, then the gff file obtained would be signalpOut.gff3
 	signalp -fasta $prot -org $org -format short -gff3 -prefix signalpOut
 }
 
