b0d7a0b4cf3b8ee50462a024e8822af37c264b4f
Sharma, Siddhartha <ssharma435@gatech.edu>
2019-03-26 17:43:10 -0400

    Update pilerforall.sh

--- pilerforall.sh
+++ pilerforall.sh
@@ -1,40 +1,22 @@
-#!/bin/bash
-
-while getopts "i:o:h" option
+while getopts "i:h" option
 do
         case $option in
              i) input=$OPTARG;;
-             o) output=$OPTARG;;
-             h) echo "pilercr.bash -i <Input Directory Name> -o <Output Directory Name>"
+             h) echo "pilercr.bash -i <Input Directory Name - Only .fasta files>"
                 echo "Required Arguments"
-                echo "-i Name of Input file directory. All Input files should be in nucleotide fasta format. Script assumes files are only in .fna or .fasta format"
-                echo "-o Name of Output file directory. Please make sure directory exists before running script. Gff files will be stored here. "
+                echo "-i Name of Input file directory. All Input files should be in nucleotide fasta format. Script assumes files are only in .fasta format"
                 exit 0;
         esac
 done
 
-cd $input
-for f in *.fasta
+mkdir pilerout
+for file in $input*.fasta
   do
+  f="$(echo $file | rev | cut -d/ -f1 | rev )"
   echo $f
-  pilercr -in $f -out $output/temp_piler_out -quiet -noinfo
+  pilercr -in $file -out pilerout/temp_piler_out -quiet -noinfo
   f2=${f%.fasta}.gff
-  /home/siddhartha/input/pilertogff.py $output/temp_piler_out $output/$f2
-  rm $output/temp_piler_out
+  python pilertogff.py pilerout/temp_piler_out pilerout/$f2
+  rm pilerout/temp_piler_out
   done
-
-
-for f in *.fna
-  do
-  echo $f
-  pilercr -in $f -out $output/temp_piler_out -quiet -noinfo
-  f2=${f%.fna}.gff
-  /home/siddhartha/input/pilertogff.py $output/temp_piler_out $output/$f2
-  rm $output/temp_piler_out
-  done
-
-
-#pilercr -in $input -out temp_piler_out -quiet -noinfo
-#/home/siddhartha/input/pilertogff.py temp_piler_out $output
-#rm temp_piler_out
 
