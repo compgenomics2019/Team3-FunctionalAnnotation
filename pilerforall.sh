3b7d2a395b2d248100791e5668db24b860a5bd28
Sharma, Siddhartha <ssharma435@gatech.edu>
2019-03-26 18:03:20 -0400

    Update pilerforall.sh

--- pilerforall.sh
+++ pilerforall.sh
@@ -2,7 +2,7 @@ while getopts "i:h" option
 do
         case $option in
              i) input=$OPTARG;;
-             h) echo "pilercr.bash -i <Input Directory Name - Only .fasta files>"
+             h) echo "pilerforall.bash -i <Input Directory Name - Only .fasta files>"
                 echo "Required Arguments"
                 echo "-i Name of Input file directory. All Input files should be in nucleotide fasta format. Script assumes files are only in .fasta format"
                 exit 0;
@@ -13,7 +13,7 @@ mkdir pilerout
 for file in $input*.fasta
   do
   f="$(echo $file | rev | cut -d/ -f1 | rev )"
-  echo $f
+  #echo $f
   pilercr -in $file -out pilerout/temp_piler_out -quiet -noinfo
   f2=${f%.fasta}.gff
   python pilertogff.py pilerout/temp_piler_out pilerout/$f2
