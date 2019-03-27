b3b363bd5f598effbf0da38966e585621f5cb32e
George Gruenhagen <ggruenhagen3@gatech.edu>
2019-03-26 20:05:06 -0400

    Removed old stuff

--- pipeline.bash
+++ pipeline.bash
@@ -2,13 +2,11 @@
 
 #Usage statement
 usage () {
-	echo "Usage: rename.bash -a <assembled_genome> -c <card.json> -m <card_model> -g <input_gff> -n <input_nucl> -p <input_prot> -o <output_name> -t <org_type>
+	echo "Usage: rename.bash -c <card.json> -m <card_model> -g <input_gff> -p <input_prot> -o <output_name> -t <org_type>
 	Required Arguments:
-	-a assembled genome
 	-c path/to/card.json
 	-m path/to/model
 	-g name of the input gff file that contains predicted genes
-	-n name of the input fna file that contains predicted genes
 	-p name of the input faa file that contains predicted proteins
 	-o desired name of the annotated output files. This will included annotated nucleotide, protein, and gff files.
 	-t Type of Organism - Required for SignalP. Archaea: 'arch', Gram-positive: 'gram+', Gram-negative: 'gram-' or Eukarya: 'euk'
@@ -26,19 +24,15 @@ check_for_help() {
 get_input() {
 	check_for_help "$1"
 	
-	assembledGenome=""
 	gff=""
-	nucl=""
 	prot=""
 	out=""
 	c=""
 	m=""
 	org=""
-	while getopts "a:g:n:p:o:c:m:t:h" opt; do
+	while getopts "g:p:o:c:m:t:h" opt; do
 		case $opt in
-		a ) assembledGenome=$OPTARG;;
 		g ) gff=$OPTARG;;
-		n ) nucl=$OPTARG;;
 		p ) prot=$OPTARG;;
 		o ) out=$OPTARG;;
 		c ) c=$OPTARG;;
@@ -60,11 +54,6 @@ check_files() {
 		usage
 		exit 1
 	fi
-	if [ -z "$nucl" ] || [ ! -f "$nucl" ]; then
-		echo "A valid input fna file must be given. Exiting the program."
-		usage
-		exit 1
-	fi
 	if [ -z "$prot" ] || [ ! -f "$prot" ]; then
 		echo "A valid input faa file must be given. Exiting the program."
 		usage
@@ -78,15 +67,9 @@ check_files() {
 	
 	# Check to see if the input files have the correct extensions
 	gffExtension="${gff##*.}"
-	nuclExtension="${nucl##*.}"
 	protExtension="${prot##*.}"
 	if [ "$gffExtension" != "gff" ] && [ "$gff" != "gff3" ]; then
 		echo "The input gff file does not have a gff or gff3 extension. Exiting the program."
-		usage
-		exit 1
-	fi
-	if [ "$nuclExtension" != "fna" ] && [ "$nuclExtension" != "fasta" ] && [ "$nuclExtension" != "fa" ]; then
-		echo "The input nucleotide sequences do not have a fna, fasta, nor fa file extension. Exiting the program."
 		usage
 		exit 1
 	fi
@@ -148,22 +131,12 @@ merge() {
 	cat eggNOG_out card_out door2_out vfdb_out intPro_out piler_out signalpOut.gff3 > final_out.gff
 }
 
-annotate_fasta() {
-	# Take the annotations from the gff file and give those annotation to the fasta files
-	# ie the nucleotide sequences and protein sequences
-	gff=fina_out.gff
-	cat $gff | awk -F'\t' '{print $1 "\t" $2 "\t" $1 "|" $2 "|" $3 "|" $4 "|" $5 "|" $6 "|" $7 "|" $8 "|" $9 "\t" $4 "\t" $5 "\t" $6 "\t" $7 "\t" $8 "\t" $9}' > temp.gff
-	bedtools getfasta -fi $assembledGenome -bed temp.gff -fo "$out"".fna" -name
-	transeq "$out"".fna" "$out"".faa" -frame=1 -trim -sformat pearson
-}
-
 main() {
 	get_input "$@"
 	check_files
 	ab_initio
 	homology
 	merge
-	annotate_fasta
 }
 
 
